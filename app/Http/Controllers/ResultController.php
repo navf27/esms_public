ecd89974a006f7af2368a67e6fe8863380f318fa
navf27 <bronaufalaji@gmail.com>
2024-01-18 21:50:22 +0700

    first commit

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -3,27 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\Log;
-use Illuminate\Http\Request;
-use PDO;
 use App\Models\Surat;
+use Illuminate\Http\Request;
 
 class ResultController extends Controller
 {
     //index
-    public function index(){
+    public function index()
+    {
         $key = session()->get('kata_kunci');
 
-        $mail = Surat::where('nosurat' , 'like', '%'. $key .'%')
-        ->orWhere('hal', 'like', '%' . $key . '%')
-        ->orWhere('kepada', 'like', '%' . $key . '%')
-        ->orWhere('dari', 'like', '%' . $key . '%')
-        ->orWhere('noagenda', 'like', '%' . $key . '%')
-        ->paginate(10);
+        $mail = Surat::where('nosurat', 'like', '%' . $key . '%')
+            ->orWhere('hal', 'like', '%' . $key . '%')
+            ->orWhere('kepada', 'like', '%' . $key . '%')
+            ->orWhere('dari', 'like', '%' . $key . '%')
+            ->orWhere('noagenda', 'like', '%' . $key . '%')
+            ->paginate(10);
 
         return view('halaman2', compact('mail'));
     }
 
-    public function search(Request $request){
+    public function search(Request $request)
+    {
         $key = $request->search;
 
         $log = Log::create([
@@ -32,9 +33,9 @@ class ResultController extends Controller
             'alamat' => $request->alamat,
             'jenis_kelamin' => $request->jenis,
             'kata_kunci' => $key,
-            'ip' => $request->ip()
+            'ip' => $request->ip(),
         ]);
 
         return redirect()->route('halaman2.index')->with(['nama' => $log->nama, 'alamat' => $log->alamat, 'hp' => $log->hp, 'jenis_kelamin' => $log->jenis_kelamin, 'kata_kunci' => $key]);
-    }
+    } //
 }
